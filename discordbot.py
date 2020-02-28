ac0fd83090c93a7d0f5f25f01756e7ae41518a4e
Matteo Villosio <sirjack9601@gmail.com>
2020-02-28 12:03:30 +0100

    should work now

--- discordbot.py
+++ discordbot.py
@@ -1,13 +1,14 @@
-import os
-
 import discord
+import recapper
 
 client = discord.Client()
 token = open('token.txt', 'r').readline()
 
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+
 
 @client.event
 async def on_message(message):
@@ -15,10 +16,17 @@ async def on_message(message):
         return
 
     if message.content.startswith('!recap'):
-        splitted_message = message.content.split(" ")
-        if len(splitted_message) != 3:
-            await message.channel.send('Wrong syntax, send the link as "!recap <your article> <a float between 0 and 1 to choose recap lenght, eg 0.3>')
+        [cmd, url, perc] = split_message = message.content.split(" ")
+        if len(split_message) == 2 or len(split_message) == 3:
+            r = recapper.Recapper(url=url)
+            r.process()
+            perc = 0.22 if len(split_message) == 2 else perc = perc
+            recap = r.summarize(perc=perc)
+            await message.channel.send(recap)
         else:
+            await message.channel.send(
+                'Wrong syntax, send the link as "!recap <your article> <optional but recommended: a float between 0 '
+                'and 1 to choose recap length, eg 0.3>')
 
 
 client.run(token)
